524eb66be9cef4bad870e00ba89bbae116f5708e
Ajay o s <akhubsmaster@gmail.com>
2022-03-24 10:58:40 +0530

    Create do-while.c

---
+++ do-while/do-while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main ()
+{
+   int n,i=1;
+   printf("ENTER A NUMBER : ");
+   scanf("%d",&n);
+   do 
+    {
+      printf("%d, ",i);
+      i=i+1;  // or use i+=1;
+   }
+  while(i<=n);
+  }
