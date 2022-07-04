ac0049b4432b72eaf5c8aca13b6295bbe3e8dc1e
Ajay o s <akhubsmaster@gmail.com>
2022-07-04 13:37:09 +0000

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- exam/README.md
+++ exam/README.md
@@ -1,2 +1,4 @@
-# [Android Application](https://play.google.com/store/apps/details?id=ru.iiec.pydroid3&hl=en_GB&gl=IN)
+# [Pydroid3](https://play.google.com/store/apps/details?id=ru.iiec.pydroid3&hl=en_GB&gl=IN)
+
+
 
---
+++ exam/add1.py
@@ -0,0 +1,13 @@
+num_1 = int(input("Enter 1 number : "));
+num_2 = int(input("Enter 2 number : "));
+
+add = num_1 + num_2;
+sub = num_1 -num_2;
+mul = num_1 * num_2;
+div = num_1 / num_2;
+lk = num_1 % num_2;
+print("{0} + {1} = {2}".format(num_1,num_2,add));
+print("{0} - {1} = {2}".format(num_1,num_2,sub));
+print("{0} * {1} = {2}".format(num_1,num_2,mul));
+print("{0} / {1} = {2}".format(num_1,num_2,div));
+print("{0} % {1} = {2} , {3}".format(num_1,num_2,num_1%num_2,lk));
