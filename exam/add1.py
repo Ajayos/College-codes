59034a96fe04b25d9dffcf5a06ba2d5e76098a9f
Ajay o s <akhubsmaster@gmail.com>
2022-07-04 15:22:23 +0000

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- exam/add1.py
+++ exam/add1.py
@@ -5,9 +5,9 @@ add = num_1 + num_2;
 sub = num_1 -num_2;
 mul = num_1 * num_2;
 div = num_1 / num_2;
-lk = num_1 % num_2;
-print("{0} + {1} = {2}".format(num_1,num_2,add));
-print("{0} - {1} = {2}".format(num_1,num_2,sub));
-print("{0} * {1} = {2}".format(num_1,num_2,mul));
-print("{0} / {1} = {2}".format(num_1,num_2,div));
-print("{0} % {1} = {2} , {3}".format(num_1,num_2,num_1%num_2,lk));
+
+
+print("add = ",add);
+print("sub = ",sub);
+print("mul = ",mul);
+print("div = ",div);
