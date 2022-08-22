5fdc162d765aaef983e6e904b149ae2f564aedb1
Ajay o s <akhubsmaster@gmail.com>
2022-08-22 14:18:57 +0000

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- EXAM/2d_array.c
+++ EXAM/2d_array.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-    int a[5][5], b[5][5], r, c, i, j;
+    int a[5][5], r, c, i, j;
     printf("Enter r and c : ");
     scanf("%d %d",&r,&c);
-    printf("Enter elements a : ");
+    printf("Enter elements : ");
     for(i=0; i<r; i++)
     {
         for(j=0; j<c; j++)
@@ -12,23 +12,16 @@ void main()
             scanf("%d",&a[i][j]);
         }
     }
-    printf("\nEnter elements b : ");
+    printf("\nElements are : \n");
     for(i=0; i<r; i++)
     {
         for(j=0; j<c; j++)
         {
-            scanf("%d",&b[i][j]);
-        }
-    }
-    printf("\naddition output : \n");
-    for(i=0; i<r; i++)
-    {
-        for(j=0; j<c; j++)
-        {
-            printf("%d ",a[i][j]+b[i][j]);
+            printf("%d ",a[i][j]);
         }
         printf("\n");
     }
+
 }
 
 /**
